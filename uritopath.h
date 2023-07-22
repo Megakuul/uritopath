ee0297a1e4ed7ea842162ae4c05e0d96be2a0f40
linus <megakuul@gmx.ch>
2023-07-22 13:32:07 +0200

    Added support for C++ linker

--- uritopath.h
+++ uritopath.h
@@ -3,6 +3,10 @@
 
 #include <string.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /**
  * Converts a file uri in format "file://C:\Users\User%201" or "file:///home/User%201"
  * to a system path like "C:\Users\User 1" or "/home/User 1"
@@ -20,5 +24,9 @@ char* dyn_decuri(const char *uri);
  * @return Returns a pointer to the encoded path or NULL if allocation fails
 */
 char* dyn_encpath(const char* path);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
